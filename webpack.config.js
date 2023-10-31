21228603d08117149e91fd7a2c9d536bd11a9f07
Eduardo Duarte <dudu_duarte@hotmail.com>
2023-10-30 23:15:27 -0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+module.exports = {
+    mode: "production",
+    entry: "./src/index.tsx",
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx|tsx|ts|scss)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                loader: 'file-loader'
+            }
+        ],
+    },
+    resolve: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+    },
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "build"),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+        template: "./public/index.html",
+        inject:true,
+        filename: 'index.html'
+        }),
+        new MiniCssExtractPlugin(),
+        new CopyPlugin({
+            patterns: [
+                { from: "node_modules/scichart/_wasm/scichart2d.data", to: "" },
+                { from: "node_modules/scichart/_wasm/scichart2d.wasm", to: "" },
+            ],
+        }),
+    ],
+    devServer: {
+        client: {
+            overlay: {
+                warnings: false,
+                errors: true,
+            },
+        },
+    },
+};
